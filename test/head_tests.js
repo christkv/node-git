8caf07bbce565504dffd0d8bb0aa87e00350a61e
Christian Amor Kvalheim <christian.kvalheim@xing.com>
2010-06-23 17:11:09 +0200

    Working on file index

--- test/head_tests.js
+++ test/head_tests.js
@@ -124,94 +124,108 @@ suite.addTests({
   //   })    
   // },
   
-  // -------------------------------------- PASSING
-  
-  // Heads
-  "Should correctly return the current head":function(assert, finished) {
-    new Repo("./test/dot_git", {is_bare:true}, function(err, repo) {
-      repo.head(function(err, head) {
-        assert.ok(head instanceof Head);
-        assert.equal('master', head.name);
-  
-        repo.commits(head.name, function(err, commits) {
-          assert.equal('ca8a30f5a7f0f163bbe3b6f0abf18a6c83b0687a', commits[0].id);
-          finished();
-        });
-      })      
-    });
-  },
-  
-  "Should correcty return array of head objects":function(assert, finished) {
-    new Repo("./test/..", function(err, repo) {
-      repo.heads(function(err, heads) {
-        heads.forEach(function(head) {
-          assert.ok(head instanceof Head);        
-        })
-        finished();
-      })      
-    });
-  },
-  
-  "Should populate head data":function(assert, finished) {
-    new Repo("./test/dot_git", {is_bare:true}, function(err, repo) {
-      repo.heads(function(err, heads) {
-        var head = heads[1];
-  
-        assert.equal('test/master', head.name);        
-        assert.equal('2d3acf90f35989df8f262dc50beadc4ee3ae1560', head.commit.id);
-        finished();
-      })      
-    });
-  },
-  
-  // Commits
-  
-  "Should correctly fetch commits":function(assert, finished) {
-    new Repo("./..", {is_bare:true}, function(err, repo) {
-      Git.prototype.rev_list = function(options, string, callback) { callback(null, fixture('rev_list')); };
-
-      repo.commits('master', 10, function(err, commits) {
-        var commit = commits[0];
-
-        assert.equal('4c8124ffcf4039d292442eeccabdeca5af5c5017', commit.id);
-        var p_commit_ids = commit.parents.map(function(p_commit) { return p_commit.id; });
-        assert.deepEqual(["634396b2f541a9f2d58b00be1a07f0c358b999b3"], p_commit_ids);
-
-        assert.equal('672eca9b7f9e09c22dcb128c283e8c3c8d7697a4', commit.tree.id);
-        assert.equal('Tom Preston-Werner', commit.author.name);
-        assert.equal('tom@mojombo.com', commit.author.email);
-        assert.deepEqual(new Date(1191999972*1000), commit.authored_date);
-        
-        assert.equal('Tom Preston-Werner', commit.comitter.name);
-        assert.equal('tom@mojombo.com', commit.comitter.email);
-        assert.deepEqual(new Date(1191999972*1000), commit.committed_date);
-        assert.equal('implement Grit#heads', commit.message);
-
-        // Check commit 1
-        assert.deepEqual([], commits[1].parents)
-
-        // Check commit 2
-        p_commit_ids = commits[2].parents.map(function(p_commit) { return p_commit.id; });
-        assert.deepEqual(["6e64c55896aabb9a7d8e9f8f296f426d21a78c2c", "7f874954efb9ba35210445be456c74e037ba6af2"], p_commit_ids);
-        
-        assert.equal("Merge branch 'site'\n\n  * Some other stuff\n  * just one more", commits[2].message);
-        assert.equal("Merge branch 'site'", commits[2].short_message);
-        finished();
-      })      
-    });
-  },
-  
-  // -------------------------------------- PASSING END  
-  
-  // // commit count
-  // "Should correctly retrieve the commit count":function(assert, finished) {
-  //   var repo = new Repo("./..", {is_bare:true});
-  //   repo.commit('634396b2f541a9f2d58b00be1a07f0c358b999b3', function(err, commit) {
-  //     assert.equals('634396b2f541a9f2d58b00be1a07f0c358b999b3', commit.id);
-  //     finished();
-  //   })
-  // }
+  // // -------------------------------------- PASSING
   // 
+  // // Heads
+  // "Should correctly return the current head":function(assert, finished) {
+  //   new Repo("./test/dot_git", {is_bare:true}, function(err, repo) {
+  //     repo.head(function(err, head) {
+  //       assert.ok(head instanceof Head);
+  //       assert.equal('master', head.name);
+  // 
+  //       repo.commits(head.name, function(err, commits) {
+  //         assert.equal('ca8a30f5a7f0f163bbe3b6f0abf18a6c83b0687a', commits[0].id);
+  //         finished();
+  //       });
+  //     })      
+  //   });
+  // },
+  // 
+  // "Should correcty return array of head objects":function(assert, finished) {
+  //   new Repo("./test/..", function(err, repo) {
+  //     repo.heads(function(err, heads) {
+  //       heads.forEach(function(head) {
+  //         assert.ok(head instanceof Head);        
+  //       })
+  //       finished();
+  //     })      
+  //   });
+  // },
+  // 
+  // "Should populate head data":function(assert, finished) {
+  //   new Repo("./test/dot_git", {is_bare:true}, function(err, repo) {
+  //     repo.heads(function(err, heads) {
+  //       var head = heads[1];
+  // 
+  //       assert.equal('test/master', head.name);        
+  //       assert.equal('2d3acf90f35989df8f262dc50beadc4ee3ae1560', head.commit.id);
+  //       finished();
+  //     })      
+  //   });
+  // },
+  // 
+  // // Commits
+  // 
+  // "Should correctly fetch commits":function(assert, finished) {
+  //   new Repo("./..", {is_bare:true}, function(err, repo) {
+  //     Git.prototype.rev_list = function(options, string, callback) { callback(null, fixture('rev_list')); };
+  // 
+  //     repo.commits('master', 10, function(err, commits) {
+  //       var commit = commits[0];
+  // 
+  //       assert.equal('4c8124ffcf4039d292442eeccabdeca5af5c5017', commit.id);
+  //       var p_commit_ids = commit.parents.map(function(p_commit) { return p_commit.id; });
+  //       assert.deepEqual(["634396b2f541a9f2d58b00be1a07f0c358b999b3"], p_commit_ids);
+  // 
+  //       assert.equal('672eca9b7f9e09c22dcb128c283e8c3c8d7697a4', commit.tree.id);
+  //       assert.equal('Tom Preston-Werner', commit.author.name);
+  //       assert.equal('tom@mojombo.com', commit.author.email);
+  //       assert.deepEqual(new Date(1191999972*1000), commit.authored_date);
+  //       
+  //       assert.equal('Tom Preston-Werner', commit.comitter.name);
+  //       assert.equal('tom@mojombo.com', commit.comitter.email);
+  //       assert.deepEqual(new Date(1191999972*1000), commit.committed_date);
+  //       assert.equal('implement Grit#heads', commit.message);
+  // 
+  //       // Check commit 1
+  //       assert.deepEqual([], commits[1].parents)
+  // 
+  //       // Check commit 2
+  //       p_commit_ids = commits[2].parents.map(function(p_commit) { return p_commit.id; });
+  //       assert.deepEqual(["6e64c55896aabb9a7d8e9f8f296f426d21a78c2c", "7f874954efb9ba35210445be456c74e037ba6af2"], p_commit_ids);
+  //       
+  //       assert.equal("Merge branch 'site'\n\n  * Some other stuff\n  * just one more", commits[2].message);
+  //       assert.equal("Merge branch 'site'", commits[2].short_message);
+  //       finished();
+  //     })      
+  //   });
+  // },
+  // 
+  // // -------------------------------------- PASSING END  
+  
+  // commit count
+  "Should correctly retrieve the commit":function(assert, finished) {
+    new Repo("./test/dot_git", {is_bare:true}, function(err, repo) {
+      repo.commit('634396b2f541a9f2d58b00be1a07f0c358b999b3', function(err, commit) {
+        assert.equal('634396b2f541a9f2d58b00be1a07f0c358b999b3', commit.id);
+        finished();
+      })      
+    });
+  },
+  
+  "Should correctly retrieve the commit count":function(assert, finished) {
+    // new Repo("./..", {is_bare:true}, function(err, repo) {
+    //   repo.commit('634396b2f541a9f2d58b00be1a07f0c358b999b3', function(err, commit) {
+    //     sys.puts("================= err: " + err)
+    //     sys.puts("================= commit: " + sys.inspect(commit))
+    //     
+    //     assert.equal('634396b2f541a9f2d58b00be1a07f0c358b999b3', commit.id);
+    //     finished();
+    //   })      
+    // });    
+    finished();
+  }
+  
   // // tree
   // "Should correctly retrieve the repo tree":function(assert, finished) {
   //   var repo = new Repo("./..", {is_bare:true});
