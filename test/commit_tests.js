52e39b0d5830f192f1f33ea5c0c893d7c7cf9d22
Christian Amor Kvalheim <christian.kvalheim@xing.com>
2010-07-05 18:10:56 +0200

    Adding more commit tests

--- test/commit_tests.js
+++ test/commit_tests.js
@@ -95,6 +95,79 @@ suite.addTests({
         finished();
       });
     });    
+  },
+  
+  "Test diff with files":function(assert, finished) {
+    new Repo("./test/dot_git", {is_bare:true}, function(err, repo) {
+      repo.git.diff = function(a, b, c, d, callback) {
+          assert.equal(true, a['full_index']);
+          assert.equal('59ddc32', b);        
+          assert.equal('--', c);        
+          assert.equal('lib', d);
+          callback(null, fixture('diff_f'));
+        }
+        
+      // Fetch the diff
+      Commit.diff(repo, '59ddc32', ["lib"], function(err, diffs) {
+        assert.equal(1, diffs.length);
+        assert.deepEqual('lib/grit/diff.rb', diffs[0].a_path);
+        finished();
+      });
+    });        
+  },
+  
+  "Test diff with two commits and files":function(assert, finished) {
+    new Repo("./test/dot_git", {is_bare:true}, function(err, repo) {
+      repo.git.diff = function(a, b, c, d, e, callback) {
+          assert.equal(true, a['full_index']);
+          assert.equal('59ddc32', b);
+          assert.equal('13d27d5', c);
+          assert.equal('--', d);
+          assert.equal('lib', e);
+          callback(null, fixture('diff_2f'));
+        }
+        
+      // Fetch the diff
+      Commit.diff(repo, '59ddc32', '13d27d5', ["lib"], function(err, diffs) {
+        assert.equal(1, diffs.length);
+        assert.deepEqual('lib/grit/commit.rb', diffs[0].a_path);
+        finished();
+      });
+    });    
+  },
+  
+  // diffs
+  "Test diffs":function(assert, finished) {
+    new Repo("./test/dot_git", {is_bare:true}, function(err, repo) {
+      repo.git.diff = function() {
+          var args = Array.prototype.slice.call(arguments, 0);
+          // Pop the callback
+          var callback = args.pop();
+          callback(null, fixture('diff_p'));
+        }
+        
+      // Fetch the diff
+      var commit = new Commit(repo, '91169e1f5fa4de2eaea3f176461f5dc784796769');
+      commit.diffs(function(err, diffs) {
+        assert.equal(15, diffs.length);
+        
+        assert.equal('.gitignore', diffs[0].a_path);
+        assert.equal('.gitignore', diffs[0].b_path);
+        assert.equal('4ebc8aea50e0a67e000ba29a30809d0a7b9b2666', diffs[0].a_blob.id);
+        assert.equal('2dd02534615434d88c51307beb0f0092f21fd103', diffs[0].b_blob.id);
+        assert.equal('100644', diffs[0].b_mode);
+        assert.equal(false, diffs[0].new_file);
+        assert.equal(false, diffs[0].deleted_file);
+        assert.equal("--- a/.gitignore\n+++ b/.gitignore\n@@ -1 +1,2 @@\n coverage\n+pkg", diffs[0].diff);
+        
+        assert.equal('lib/grit/actor.rb', diffs[5].a_path);
+        assert.equal(null, diffs[5].a_blob);
+        assert.equal('f733bce6b57c0e5e353206e692b0e3105c2527f4', diffs[5].b_blob.id);
+        assert.equal(true, diffs[5].new_file);
+        
+        finished();
+      });
+    });        
   }
 });
 
