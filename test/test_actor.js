a3bf09bef7e187da75662258d1dfcff8c4bdc118
Christian Amor Kvalheim <christian.kvalheim@xing.com>
2010-07-06 17:06:58 +0200

    Adde blame function and tests

---
+++ test/test_actor.js
@@ -0,0 +1,53 @@
+require.paths.unshift("./spec/lib", "./lib", "./external-libs/node-httpclient/lib", "./external-libs/node-xml/lib",
+  "./external-libs/node-async-testing");
+
+TestSuite = require('async_testing').TestSuite,
+  sys = require('sys'),
+  Repo = require('git/repo').Repo,
+  fs = require('fs'),
+  Actor = require('git/actor').Actor;
+
+var suite = exports.suite = new TestSuite("actor tests");
+
+var fixture = function(name, trim) {
+  return trim ? fs.readFileSync("./test/fixtures/" + name, 'ascii').trim() : fs.readFileSync("./test/fixtures/" + name, 'ascii');
+}
+
+suite.addTests({  
+  "Should create actor from string seperating name and email":function(assert, finished) {
+    var actor = Actor.from_string("Tom Werner <tom@example.com>");
+    assert.equal("Tom Werner", actor.name);
+    assert.equal("tom@example.com", actor.email);
+    finished();
+  },
+  
+  "Should create actor from string only containing name":function(assert, finished) {
+    var actor = Actor.from_string("Tom Werner");
+    assert.equal("Tom Werner", actor.name);
+    assert.equal(null, actor.email);
+    finished();
+  },
+  
+  "Should correctly return name when calling toString()":function(assert, finished) {
+    var actor = Actor.from_string("Tom Werner <tom@example.com>");
+    assert.equal(actor.name, actor.toString());
+    finished();
+  }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- test/test_all.js
+++ test/test_all.js
@@ -9,3 +9,5 @@ require('test_head').suite.runTests(function() {});
 require('test_diff').suite.runTests(function() {});
 require('test_file_index').suite.runTests(function() {});
 require('test_commit').suite.runTests(function() {});
+require('test_actor').suite.runTests(function() {});
+require('test_blame').suite.runTests(function() {});
