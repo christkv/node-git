f7bbee6c7c173e6e1a78b6886c48715032c61635
Christian Amor Kvalheim <christian.kvalheim@xing.com>
2010-07-09 12:45:30 +0200

    Added config functions

---
+++ test/test_commit_write.js
@@ -0,0 +1,66 @@
+require.paths.unshift("./spec/lib", "./lib", "./external-libs/node-httpclient/lib", "./external-libs/node-xml/lib",
+  "./external-libs/node-async-testing");
+
+TestSuite = require('async_testing').TestSuite,
+  sys = require('sys'),
+  Repo = require('git/repo').Repo,
+  fs = require('fs'),
+  Commit = require('git/commit').Commit,
+  Blob = require('git/blob').Blob;
+
+var suite = exports.suite = new TestSuite("commit write tests");
+
+var fixture = function(name, trim) {
+  return trim ? fs.readFileSync("./test/fixtures/" + name, 'ascii').trim() : fs.readFileSync("./test/fixtures/" + name, 'ascii');
+}
+
+suite.addTests({  
+  "Should correctly fetch commit index":function(assert, finished) {
+    new Repo("/Users/christian.kvalheim/coding/checkouts/grit", {is_bare:true}, function(err, repo) {  
+      repo.git.commit = function() {
+          var args = Array.prototype.slice.call(arguments, 0);
+          // Pop the callback
+          var callback = args.pop();
+          callback(null, fixture('commit'));
+        }
+        
+      repo.commit_index('my message', function(err, results) {
+        assert.ok(results.indexOf('Created commit') != -1)
+        finished();
+      })
+    });    
+  },
+  
+  "Should correctly commit all":function(assert, finished) {
+    new Repo("/Users/christian.kvalheim/coding/checkouts/grit", {is_bare:true}, function(err, repo) {  
+      repo.git.commit = function() {
+          var args = Array.prototype.slice.call(arguments, 0);
+          // Pop the callback
+          var callback = args.pop();
+          callback(null, fixture('commit'));
+        }
+        
+      repo.commit_all('my message', function(err, results) {
+        assert.ok(results.indexOf('Created commit') != -1)
+        finished();
+      })
+    });        
+  }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ test/test_config.js
@@ -0,0 +1,83 @@
+require.paths.unshift("./spec/lib", "./lib", "./external-libs/node-httpclient/lib", "./external-libs/node-xml/lib",
+  "./external-libs/node-async-testing");
+
+TestSuite = require('async_testing').TestSuite,
+  sys = require('sys'),
+  Repo = require('git/repo').Repo,
+  fs = require('fs'),
+  Commit = require('git/commit').Commit,
+  Blob = require('git/blob').Blob;
+
+var suite = exports.suite = new TestSuite("config tests");
+
+var fixture = function(name, trim) {
+  return trim ? fs.readFileSync("./test/fixtures/" + name, 'ascii').trim() : fs.readFileSync("./test/fixtures/" + name, 'ascii');
+}
+
+suite.addTests({  
+  "Should correctly return an assoc array":function(assert, finished) {
+    new Repo("/Users/christian.kvalheim/coding/checkouts/grit", {is_bare:true}, function(err, repo) {  
+      repo.git.config = function() {
+          var args = Array.prototype.slice.call(arguments, 0);
+          // Pop the callback
+          var callback = args.pop();
+          callback(null, fixture('simple_config'));
+        }
+      
+      repo.config(function(err, config) {
+        assert.equal("git://github.com/mojombo/grit.git", config.fetch("remote.origin.url"));
+        assert.equal(null, config.fetch("unknown"));
+        assert.equal("false", config.fetch("core.bare"));
+        assert.equal("default", config.fetch("unknown", "default"));
+        finished();
+      });
+    });        
+  },
+  
+  "Should correctly set value":function(assert, finished) {
+    new Repo("/Users/christian.kvalheim/coding/checkouts/grit", {is_bare:true}, function(err, repo) {  
+      repo.git.config = function() {        
+          var args = Array.prototype.slice.call(arguments, 0);                    
+          // Pop the callback
+          var callback = args.pop();          
+          var options = args.shift();
+          // Just return if we are done
+          if(options['list']) return callback(null, fixture('simple_config'));
+
+
+          var value = args.shift();
+          var default_value = args.shift();
+          
+          assert.deepEqual({}, options);
+          assert.equal('unknown', value);
+          assert.equal('default', default_value);          
+          callback(null, fixture('simple_config'));
+        }
+      
+      
+      repo.config(function(err, config) {
+        config.set("unknown", "default", function(err, result) {
+          assert.ok(!err);
+          finished();
+        })        
+      });
+    });            
+  }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
