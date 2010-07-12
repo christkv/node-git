46bb858cffc13e6f83f12fa4a514ce8f35cd6e61
Christian Amor Kvalheim <christian.kvalheim@xing.com>
2010-07-12 17:46:56 +0200

    Added remote call to repo

---
+++ test/test_remote.js
@@ -0,0 +1,47 @@
+require.paths.unshift("./spec/lib", "./lib", "./external-libs/node-httpclient/lib", "./external-libs/node-xml/lib",
+  "./external-libs/node-async-testing");
+
+var TestSuite = require('async_testing').TestSuite,
+  sys = require('sys'),
+  fs = require('fs'),
+  Repo = require('git/repo').Repo,
+  Merge = require('git/merge').Merge;
+
+var suite = exports.suite = new TestSuite("remote tests");
+
+var fixture = function(name, trim) {
+  return trim ? fs.readFileSync("./test/fixtures/" + name, 'ascii').trim() : fs.readFileSync("./test/fixtures/" + name, 'ascii');
+}
+
+/**
+  Test basic node-git functionality
+**/
+suite.addTests({
+  "Should correctly parse the raw object":function(assert, finished) {
+    new Repo("/Users/christian.kvalheim/coding/checkouts/grit", {is_bare:true}, function(err, repo) {
+      repo.remotes(function(err, remotes) {
+        assert.ok(remotes.length > 0)
+        finished();
+      })
+    });        
+  }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
